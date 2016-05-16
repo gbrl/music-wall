1643319fb4b726c976c21764bff79bfc4056ad18
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-05-16 13:50:50 -0400

    Add actions and view for User model

--- app/actions.rb
+++ app/actions.rb
@@ -4,6 +4,8 @@ get '/' do
   redirect '/tracks'
 end
 
+# TRACK ACTIONS
+
 get '/tracks' do
   @tracks = Track.all
   erb :'tracks/index'
@@ -32,6 +34,35 @@ get '/tracks/:id' do
   erb :'tracks/show'
 end
 
+# USER ACTIONS
+
+get '/users' do
+  @users = User.all
+  erb :'users/index'
+end
+
+post '/users' do
+  @user = User.new(
+    username:   params[:username],
+    email:      params[:email],
+    password:   params[:password]
+  )
+  if @user.save
+    redirect '/users'
+  else
+    erb :'users/new'
+  end
+end
+
+get '/users/new' do
+  @user = User.new
+  erb :'users/new'
+end
+
+get '/users/:username' do
+  @user = User.find_by_username params[:username]
+  erb :'users/show'
+end
 
 helpers do
   def cookie_values(parameters)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :tracks
   
+  validates :username, presence: true
   validates :email, presence: true
   validates :password, presence: true
 
