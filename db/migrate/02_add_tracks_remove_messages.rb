d0684819d41f059502bf55c99a1b2fcc0f0be597
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-05-16 11:37:30 -0400

    Drop messages table (oops) and create tracks table

--- db/migrate/02_add_tracks_remove_messages.rb
+++ db/migrate/02_add_tracks_remove_messages.rb
@@ -1,5 +1,6 @@
-class AddTracks < ActiveRecord::Migration
+class AddTracksRemoveMessages < ActiveRecord::Migration
   def change
+    drop_table :messages
     create_table :tracks do |t|
       t.string :title
       t.string :author
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
 
-  create_table "messages", force: :cascade do |t|
+  create_table "tracks", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
     t.string   "url"
