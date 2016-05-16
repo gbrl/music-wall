46bbeb73034669c2a93fb048eec79ec10f875623
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-05-16 13:27:15 -0400

    Add cookie helpers

--- app/actions.rb
+++ app/actions.rb
@@ -31,3 +31,19 @@ get '/tracks/:id' do
   @track = Track.find params[:id]
   erb :'tracks/show'
 end
+
+
+helpers do
+  def cookie_values(parameters)
+    values = {}
+    parameters.each do |key, value|
+      case key
+      when 'options'
+        values[value] = true
+      else
+        values[key] = true
+      end
+    end
+    values
+  end
+end
